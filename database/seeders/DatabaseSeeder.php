1ab9b51a4b689d949cb522f0af70af833139c5a6
momar3632 <92117941+momar3632@users.noreply.github.com>
2024-07-24 16:28:36 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
             'email' => 'admin@admin.com',
             'password' => Hash::make('password'),
