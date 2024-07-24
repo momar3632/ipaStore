3e640b7b5da1401fc114033d19decbd70a81e88d
momar3632 <92117941+momar3632@users.noreply.github.com>
2024-07-24 16:37:58 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function ($router) {
+            Route::middleware('web')
+                ->namespace('App\Http\Controllers\Dashboard')
+                ->group(base_path('routes/dashboard.php'));
+
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace('App\Http\Controllers')
+                ->group(base_path('routes/api.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
